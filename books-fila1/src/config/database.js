497559268c0252a9df47847f0085b4fb136b535d
Laura Acosta <acostamarialauraa@gmail.com>
2025-06-27 10:46:20 -0300

    Se realizó la configuración de los drives

--- books-fila1/src/config/database.js
+++ books-fila1/src/config/database.js
@@ -1,1 +1,17 @@
+import express from 'express'
+import characterRoutes from "./src/routes/book.routes.js"
+import { initDB } from './src/config/database.js';
+import sequelize from './src/config/database.js';
+import dotenv from 'dotenv'; 
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api/book", bookRoutes);
+const PORT = process.env.PORT || 3000;
+
+initDB().then(()=>{
+app.listen(PORT,()=> {
+    console.log ('Servidor corriendo en http:localhost:${PORT}');
+    });
+});
