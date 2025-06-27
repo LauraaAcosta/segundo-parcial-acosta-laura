be0a139b71f56a55ca3db5936d99cd08c69ede69
Laura Acosta <acostamarialauraa@gmail.com>
2025-06-27 10:35:17 -0300

    se realizó la estructura de el servidor

---
+++ books-fila1/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bookRoutes from "./src/routes/book.routes.js";
+import { initDB } from "./src/config/database.js";
+import sequelize from './src/config/database.js';
+import dotenv from 'dotenv'; 
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/api/books", books);
+const PORT = process.env.PORT || 3000;
+
+initDB().then(() => {
+    app.listen(PORT,()=>{
+        console.log
+    });
+});
